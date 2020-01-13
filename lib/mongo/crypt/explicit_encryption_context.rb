19bfd6010efece8735b2e35a835af8f7982ee768
Emily Giurleo <e.m.giurleo@gmail.com>
2020-01-13 11:03:16 -0500

    fix specs

--- lib/mongo/crypt/explicit_encryption_context.rb
+++ lib/mongo/crypt/explicit_encryption_context.rb
@@ -58,7 +58,7 @@ module Mongo
         end
 
         binary = Binary.from_data(@options[:key_id])
-        success = Binding.mongocrypt_ctx_setopt_key_id(@ctx, binary.ref)
+        success = Binding.mongocrypt_ctx_setopt_key_id(@ctx, binary.pointer)
 
         raise_from_status unless success
       end
